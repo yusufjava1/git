245cb2be7f72300ee998d75b7ac8eb500e49d9ab
yusufjava1 <yjobe4674@gmail.com>
2025-03-18 09:39:55 +0000

    Assignment
    First assignment on personal information.

---
+++ PersonalInfo.java
@@ -0,0 +1,18 @@
+public class PersonalInfo {
+    public static void main(String[] args) {
+        String myName = "Yusupha Jobe";
+        int age = 24;
+        String gender = "M";
+        String address = "Farato";
+        int telephone = +3532150;
+        String School = "Management Development Institute";
+
+        System.out.println("My name is " + myName);
+        System.out.println("My age is " + age);
+
+        System.out.println("My gender is " + gender);
+        System.out.println("I live in Farato "+address);
+        System.out.println("Here is my contact number "+telephone);
+        System.out.println("I am a student of "+School);
+    }
+}
